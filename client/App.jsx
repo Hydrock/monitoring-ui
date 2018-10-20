e136d9b9ec4b223d91a103ed43f5d423419113ab
Вечканов Алексей Сергеевич <AVechkanov@alfabank.ru>
2018-10-20 15:08:52 +0300

    fix

--- client/App.jsx
+++ client/App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                sdgsdsdgsdg
+                Lorem Ipsum
             </div>
         )
     }
--- scripts/configs/webpack.config.js
+++ scripts/configs/webpack.config.js
@@ -1,47 +1,17 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 const path = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge'); // плагин для конкатенации конфиглв
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // вывод html
 
 const ASSETS_PATH = 'assets'; // путь для сборки всех необходимых ассетов
 const BUILD_PATH = '.build';
 
-const ARUI_TEMPLATE = require('arui-presets/webpack.base'); // базовый темплейт
-const ARUI_TS_TEMPLATE = require('arui-presets-ts/webpack'); // темплейт для обработки TS
-const ARUI_DEV_TEMPLATE = require('arui-presets/webpack.development'); // тшаблон конфига для дев разработки
-const aruiProdConfigBuilder = require('arui-presets/webpack.production-builder'); // билдер конфига для прода
-
 const CWD = process.cwd(); // текущий путь
 const IS_PRODUCTION = (process.env.NODE_ENV === 'production'); // тип сборки
-const ARUI_PROD_TEMPLATE = aruiProdConfigBuilder({ extractOptions: { publicPath: './' } }); // шаблон конфига для протакшена
 
 const QUERY = {
     name: '[name].[hash].[ext]'
 };
-
-// let webpackConfig = merge.smart(
-//     ARUI_TEMPLATE,
-//     ARUI_TS_TEMPLATE,
-//     {
-//         entry: {
-//             index: './client/index.jsx'
-//         },
-//         context: CWD,
-//         output: {
-//             path: path.resolve(CWD, BUILD_PATH, ASSETS_PATH),
-//             publicPath: '',
-//             filename: IS_PRODUCTION ? '[name].[chunkhash].js' : '[name].js'
-//         },
-//         plugins: [
-//             new HtmlWebpackPlugin({
-//                 title: 'Мониторинг приложений AO - Альфа Банк'
-//             })
-//         ]
-//     },
-//     IS_PRODUCTION ? ARUI_PROD_TEMPLATE : ARUI_DEV_TEMPLATE
-// );
-
 
 myWebPackConfig = {
     mode: 'development',
